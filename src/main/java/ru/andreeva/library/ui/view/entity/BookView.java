87add1a9817bcfdfda9f67c8f3795226419efe5c
alina <alinaandreeva130@gmail.com>
2022-05-16 21:44:03 +0700

    Скорректировано отображение вью Фонд Библиотеки

--- src/main/java/ru/andreeva/library/ui/view/entity/BookView.java
+++ src/main/java/ru/andreeva/library/ui/view/entity/BookView.java
@@ -68,9 +68,9 @@ public class BookView extends BaseEntityView<Book, Long, BookRepository> {
     protected void createColumns() {
         grid.addColumn(Book::getName).setHeader("Название").setKey("name");
         grid.addColumn(Book::getAuthor).setHeader("Автор").setKey("author");
-        grid.addColumn(book -> book.getGenre().getName()).setHeader("Жанр").setKey("genre");
-        grid.addColumn(Book::getYear).setHeader("Год издания").setKey("year");
-        grid.addColumn(Book::getPageCount).setHeader("Количество страниц").setKey("pageCount");
+        grid.addColumn(book -> book.getGenre().getName()).setHeader("Жанр").setAutoWidth(true).setKey("genre");
+        grid.addColumn(Book::getYear).setHeader("Год издания").setAutoWidth(true).setKey("year");
+        grid.addColumn(Book::getPageCount).setHeader("Количество страниц").setAutoWidth(true).setKey("pageCount");
         grid.addColumn(Book::getAgeRestriction).setHeader("Возрастное ограничение (с лет)").setKey("ageRestriction");
     }
 }
