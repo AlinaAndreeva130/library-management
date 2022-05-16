e759d00845261cb374b7f0e1c34d742c5218fac2
alina <alinaandreeva130@gmail.com>
2022-05-17 03:02:25 +0700

    Скорректировано отображение редактора читателей

--- src/main/java/ru/andreeva/library/ui/component/ReaderEditor.java
+++ src/main/java/ru/andreeva/library/ui/component/ReaderEditor.java
@@ -39,29 +39,35 @@ public class ReaderEditor extends BaseEditor<Reader, Long, ReaderRepository> {
         firstName.setRequired(true);
         firstName.setAutofocus(true);
         firstName.setValueChangeMode(ValueChangeMode.EAGER);
+        firstName.setWidthFull();
         addValidator("firstName", new StringNullableValidator());
 
         lastName = new TextField("Имя");
         lastName.setRequired(true);
         lastName.setValueChangeMode(ValueChangeMode.EAGER);
+        lastName.setWidthFull();
         addValidator("lastName", new StringNullableValidator());
 
         patronymic = new TextField("Отчество");
         patronymic.setValueChangeMode(ValueChangeMode.EAGER);
+        patronymic.setWidthFull();
 
         clazz = new TextField("Класс");
         clazz.setRequired(true);
         clazz.setValueChangeMode(ValueChangeMode.EAGER);
+        clazz.setWidthFull();
         addValidator("clazz", new IntegerValidator(1, 11, "возможно выбрать с 1-го по 11-й класс"));
 
         birthday = new DatePicker("День рождения");
         birthday.setRequired(true);
+        birthday.setWidthFull();
         addValidator("birthday",
                 new DateValidator(LocalDate.of(1900, 1, 1), LocalDate.now(),
                         "дата рождения может быть от 01.01.1900 до " + LocalDate.now().format(
                                 DateTimeFormatter.ofPattern("d.MM.y"))));
 
         contentPanel.add(firstName, lastName, patronymic, clazz, birthday);
+        contentPanel.setWidth("400px");
     }
 
     @Override
